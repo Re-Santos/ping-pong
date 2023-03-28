7c7af9bc3b4c33af9f52d3326b828303d1f5951c
Re-Santos <renata.prog@gmail.com>
2023-03-28 16:06:36 -0300

    Cria canvas

---
+++ script.js
@@ -0,0 +1,13 @@
+const canvasE1=document.querySelector("canvas"),
+      canvasCtx=canvasE1.getContext("2d")
+
+function setup(){
+    canvasE1.width = window.innerWidth
+    canvasCtx.width = window.innerWidth
+    canvasE1.height = window.innerHeight
+    canvasCtx.height = window.innerHeight
+}
+function draw(){}
+
+
+setup()
